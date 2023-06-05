376b8eff6a80ef76f90ae286e4521bd63616c294
Peter M <pmoyano@solvd.com>
2023-06-05 15:44:53 -0300

    Card buttons add and remove item

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatPrice } from "../utilities/formatPrice";
+import { useState } from "react";
 
 type StoreItemsProps = {
     id: number;
@@ -9,9 +10,10 @@ type StoreItemsProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemsProps) {
+    const quantity = 1;
     return (
         <>
-            <Card>
+            <Card className="h-100">
                 <Card.Img
                     variant="top"
                     src={imgUrl}
@@ -27,6 +29,31 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemsProps) {
                             </span>
                         </Card.Title>
                     </Card.Title>
+                    <div className="mt-auto">
+                        {quantity === 0 ? (
+                            <Button className="w-100">+ Add to cart</Button>
+                        ) : (
+                            <div
+                                className="d-flex align-items-center flex-column"
+                                style={{ gap: ".5rem" }}
+                            >
+                                <div
+                                    className="d--flex align-items-center justify-content-center"
+                                    style={{ gap: ".5rem" }}
+                                >
+                                    <Button>-</Button>
+                                    <div>
+                                        <span className="fs-3">{quantity}</span>
+                                        in cart
+                                    </div>
+                                    <Button>+</Button>
+                                </div>
+                                <Button variant="danger" size="sm">
+                                    Remove
+                                </Button>
+                            </div>
+                        )}
+                    </div>
                 </Card.Body>
             </Card>
         </>
