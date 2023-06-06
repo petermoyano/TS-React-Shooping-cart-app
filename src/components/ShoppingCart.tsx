eef8e28285eb3d0ea65481ef9f8ee3d4a9fe7d47
Peter M <pmoyano@solvd.com>
2023-06-06 18:25:05 -0300

    cartItem component

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,16 +1,25 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { v4 as uuidv4 } from "uuid";
+import { CartItem } from "./CartItem";
 
 type ShoppingCartProps = {
     isOpen: boolean;
 };
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-    const { closeCart } = useShoppingCart();
+    const { closeCart, cartItems } = useShoppingCart();
     return (
         <Offcanvas show={isOpen} onHide={closeCart} placement="end">
             <Offcanvas.Header closeButton></Offcanvas.Header>
             <Offcanvas.Title>Cart</Offcanvas.Title>
+            <Offcanvas.Body>
+                <Stack gap={3}>
+                    {cartItems.map((item) => (
+                        <CartItem key={uuidv4()} {...item} />
+                    ))}
+                </Stack>
+            </Offcanvas.Body>
         </Offcanvas>
     );
 }
